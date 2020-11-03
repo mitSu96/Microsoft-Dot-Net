7a337e920d74a545968c796a1f1b285cacf427b4
Sumit S Chaure <sumitsc.work@gmail.com>
2020-11-03 10:02:53 +0530

    Folder Containing VS Files For q2

---
+++ Assignments/Day_1 (19-10-2020)/2. Product_Of_Byte/Program.cs
@@ -0,0 +1,32 @@
+﻿//Q2. Declare 2  variable of a type byte and print product of it.
+
+
+using System;
+
+namespace Product_Of_Byte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Product of Two Byte");
+            byte x = 1, y = 20;
+            //byte z; here as z is byte error: Cannot implicitly convert type 'int' to 'byte'. An explicit conversion exists (are you missing a cast?)
+            int z;
+            z = x * y;
+
+            Console.WriteLine("Product of " + x + " & " + y + "If Variable Is Declared As Int = " + z);
+            Console.ReadLine();
+            Console.WriteLine("Typecasting To Byte:");
+            byte a = (byte)(x * y);
+            Console.WriteLine("Typecast result=" + a);
+            Console.ReadLine();
+
+        }
+    }
+}
+
+//Here In this assignment we came to know that addition/product of two byte is int or we have to explicitly store it by downcasting the sum to byte 
+//ie. byte a,b,c; a = b+c ; returns error as the product is int and cant be stored in byte we need to declare a as int to store the sum of b & c.
+//or byte a,b,c; a= (byte)(b+c);  will return the value as now the sum is downcasted to byte.
+
