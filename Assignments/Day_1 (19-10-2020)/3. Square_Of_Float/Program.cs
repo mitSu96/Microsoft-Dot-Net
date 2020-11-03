0596f80687d17489595551a586fa0d16f3d404b4
Sumit S Chaure <sumitsc.work@gmail.com>
2020-11-03 10:03:47 +0530

    Folder Containing VS Files For q3

---
+++ Assignments/Day_1 (19-10-2020)/3. Square_Of_Float/Program.cs
@@ -0,0 +1,21 @@
+﻿//Q3.  Declare a variable of a type float and print square of it after storing it in to a variable.
+
+using System;
+
+namespace _3.Square_Of_Float
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float x = 21.69f;
+            float z;
+            z = x * x;
+
+            Console.WriteLine("Square Of Float " + x + " Is");
+            Console.ReadLine();
+            Console.WriteLine(z);
+            Console.ReadLine();
+        }
+    }
+}
