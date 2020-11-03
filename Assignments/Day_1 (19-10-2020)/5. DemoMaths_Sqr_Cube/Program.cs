6e440eae72086035d49cb148432fa089e44ea7c2
Sumit S Chaure <sumitsc.work@gmail.com>
2020-11-03 10:04:24 +0530

    Folder Containing The VS Files For q5

---
+++ Assignments/Day_1 (19-10-2020)/5. DemoMaths_Sqr_Cube/Program.cs
@@ -0,0 +1,31 @@
+﻿//Q5. create a class demomaths having method Square , cube which will take float value and return square and cube of a number.
+
+using System;
+
+namespace _5.DemoMaths_Sqr_Cube
+{
+    class Program           //Entry Class Of Prg
+    {
+        static void Main(string[] args)
+        {
+            DemoMath myobj = new DemoMath(); //new is same as malloc
+            //calling method using object reference
+            float res1 = myobj.square(32.35f);
+            float res2 = myobj.cube(32.35f);
+            // + operator is overloaded it will concat
+            Console.WriteLine("The Square & Cube Of 32.5f is");
+            Console.ReadLine();
+            Console.WriteLine("square={0},cube={1}", res1, res2);
+            Console.ReadLine();
+        }
+    }
+
+    class DemoMath
+    {    //public instance method - you can call this method through object of this class
+        public float square(float x)
+        { return x * x; }
+
+        public float cube(float x)
+        { return x * x * x; }
+    }
+}
