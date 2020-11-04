77dcdb2d65c171d313c1141cf49bea64337593e5
Sumit S Chaure <sumitsc.work@gmail.com>
2020-11-04 11:06:51 +0530

    Day 2 Assignment complete VS File/Folder

---
+++ Assignments/Day_2 (20-10-2020)/6-Num_In_bytedatatype_check/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("6-Num_In_bytedatatype_check")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("6-Num_In_bytedatatype_check")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0e861252-9ee0-4ef9-ab94-570608cd188d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Assignments/Day_2 (20-10-2020)/shared_lib/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace shared_lib
+{
+    public class operation
+    {
+        public static int square(int a)
+        {
+            return a * a;
+        }
+        public static int cube(int a)
+        {
+            return a * a * a;
+        }
+    }
+}
