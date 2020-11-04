452a68bc19c187243596d099ffe3c11d8a2fcd5d
Sumit S Chaure <sumitsc.work@gmail.com>
2020-11-04 14:28:10 +0530

    Day 3 Assignment VS file/Folder

---
+++ Assignments/Day_3(21-10-2020)/2-Calculator_Struct/Class1.cs
@@ -0,0 +1,26 @@
+﻿//Q2. Accept a number from user and pass it to static method of a struct calculator it should print all factor of a number.
+
+using System;
+
+namespace _2_Calculator_Struct
+{
+    public struct calculator
+    {
+        public static void factor(int n)
+        {
+            Console.Write("\nFactors of {0} : " , n);
+            for (int i = 1; i < n; i++)
+            {
+                //Console.WriteLine("[");
+                if (n % i == 0)
+                {
+                    Console.Write(" [{0}", i + "] ,");            //also we can write as (" "+i);
+                }
+                //Console.WriteLine(n);
+            }
+            Console.WriteLine("[" + n + "].");
+        }
+    }
+}
+
+//here we can reduce two lines of code by writing i<=n which will print n at the end but would give a , at the end which looks like the factors are incomplete so we added 2 lines of extra code.
---
+++ Assignments/Day_3(21-10-2020)/3-Emp_Struct/Class1.cs
@@ -0,0 +1,21 @@
+﻿//Q3. Create struct emp having member name and salary, using constructor initialized member. Write display method which will display name and salary
+
+using System;
+
+namespace _3_Emp_Struct
+{
+    public struct emp
+    {
+        string name;
+        double salary;
+        public emp(string name, double salary)
+        {
+            this.name = name;
+            this.salary = salary;
+        }
+        public void disp_details()
+        {
+            Console.WriteLine("Employee Name : {0}\nEmployee Salary : {1}", name, salary);
+        }
+    }
+}
