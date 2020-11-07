14cd096814d4d6b349efb31d32dfc91c4f4727c1
Sumit S Chaure <sumitsc.work@gmail.com>
2020-11-07 09:48:19 +0530

    Day 3 Assignment Code ,Dlls & Exe Files.

---
+++ Day_3(21-10-2020)/4-Shared_Assesmbly_Versioning/Program.cs
@@ -0,0 +1,25 @@
+﻿//Q4. Create class library “mycaculator” having instance method  add and product which will return sum and product of two number.  Create shared assembly and use in your project. After  this add method cube  to a copy of class library “mycaculator” change the version and add it to GAC. Create a application to use this new version of library. Ensure that your both console application works.
+
+using System;
+using mycalculator;
+
+namespace _4_Shared_Assesmbly_Versioning
+{
+    class Program
+    {
+        static void Main
+            (string[] args)
+        {
+            Console.WriteLine("Part 1 of mycalculator shared assembly lib");
+            Console.WriteLine("\nEnter The 2 Numbers To Be Added & Multiplied.");
+            int a, b;
+            Console.WriteLine("\nEnter Number 1 :");
+            bool a1 = int.TryParse(Console.ReadLine() , out a);
+            Console.WriteLine("\nEnter Number 2 :");
+            bool b1 = int.TryParse(Console.ReadLine() , out b);
+            Console.WriteLine("\nThe Addition Of " + a + " & " + b + " : " + calc.add(a,b));
+            Console.WriteLine("\nThe Product Of " + a + " & " + b + " : " + calc.prod(a, b));
+            Console.ReadLine();
+        }
+    }
+}
