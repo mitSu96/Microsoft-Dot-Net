0c9a754bb602ec1a8b9255e03a78ac06bde56194
Sumit S Chaure <sumitsc.work@gmail.com>
2020-11-08 18:51:52 +0530

    Day 4 Assignment Customer Folder With sln file prnt default value when no constructor provided.

---
+++ Assignments/Day_4(22-10-2020)/Q3-Customer/Q3-Customer/Cust_lib/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Cust_lib
+{
+    public class Customer              //default access modifier internal
+    {
+        static Customer()
+        {
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine("\n****Customer Billing Desk By Razorpay Solution****\n");
+            Console.WriteLine("--------------------------------------------------");
+        }
+
+        //static int id_def=2020;
+        int id;
+        string name;
+        double billamount;
+
+        public Customer()
+        {
+            Console.WriteLine("\nInitializing default value as no constructor is used.\n");      //if you do not write constructor frame work will give you no args constructor
+        }
+        
+
+        /*public Customer(string name)
+        {
+            this.id = ++id_def;
+            this.name = name;
+        }*/
+
+            public void disp()
+            {
+                Console.WriteLine("id : {0}\nName Of Customer : {1}\nBill Amount : {2}" ,id,name,billamount);
+            }
+    }
+}
