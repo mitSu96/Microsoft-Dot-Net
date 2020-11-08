17f0dd479480bf89cc65e1dd4e76ac6a151ad6b3
Sumit S Chaure <sumitsc.work@gmail.com>
2020-11-08 18:54:22 +0530

    Day 4 Calculator Folder With Sln file Having Static Method to return operations(hardcoded)

---
+++ Assignments/Day_4(22-10-2020)/Q5-Hardcoded_Calculator/Q5-Hardcoded_Calculator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Cal_Lib;
+
+namespace Q5_Hardcoded_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press Enter To Start The Hard-Coded application");
+            Console.ReadLine();
+            float sqr = Calculator.square(22);
+            float cube = Calculator.cube(22);
+            /*double abs = Calculator.absolute(2.2);
+            double flr = Calculator.floor(2.7);
+            double ceil = Calculator.ceil(2.9);*/
+
+            Console.WriteLine("Square={0}   Cube= {1}   Absolute={2}    Floor={3}   Ceil={4}", sqr, cube, Math.Abs(-2.8), Math.Floor(2.7), Math.Ceiling(2.9));
+            Console.ReadLine();
+        }
+    }
+}
