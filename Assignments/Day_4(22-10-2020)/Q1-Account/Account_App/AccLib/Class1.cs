58fde297f8b8ffb07828dce9501c487a9963ff7b
Sumit S Chaure <sumitsc.work@gmail.com>
2020-11-08 18:50:27 +0530

    Day 4 Assignment Account Folder With sln file having private instance member

---
+++ Assignments/Day_4(22-10-2020)/Q1-Account/Account_App/AccLib/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace AccLib
+{
+    public class Account
+    {
+        static Account()                //bank name to be dispalyed as soon as app is called and only once
+        {
+            Console.WriteLine("\n****************Bank Of Unknown*****************\n");
+        }
+
+        static int id_def = 202020;           //here id_def is kept static because its value should be kept by the bank & incremented/accessed according to new user.
+        int id;                             //displayed id 
+        string name;
+        double bal = 0;                     //balance set as double & initialised to zero
+
+        public Account(string name)
+        {
+            this.id = ++id_def;
+            this.name = name;
+        }
+
+        public void deposit(double amt)
+        {
+            this.bal += amt;                //here it means balance_amt = amount_deposited + Previous_balanceamt;
+            disp();
+        }
+
+        public void withdraw(double amt)
+        {
+            if (bal <= amt)
+            {
+                Console.WriteLine("\n*******Insufficient Balance Cant Withdraw The Money From Your Account " + name + " ******");
+                Console.WriteLine("-------------------------------------------");
+            }
+            else
+            {
+                bal -= amt;
+                disp();
+            }
+        }
+
+        public void disp()
+        {
+            Console.WriteLine("-------------------------------------------");
+            Console.WriteLine("ACCOUNT NUMBER : {0}\nName Of Account Holder : {1}\nBalance : {2}", id, name, bal);
+            Console.WriteLine("-------------------------------------------");
+        }
+    }
+}
---
+++ Assignments/Day_4(22-10-2020)/Q1-Account/Account_App/Program.cs
@@ -0,0 +1,34 @@
+﻿//Q1. Create a class Account having private instance member Id, Name Balanceamt. Create method deposit who’s job is to increase Balanceamtby the amout deposited buy account holder. Create method withdraw who’s job is to decrease Balanceamtby the amout  withdrwan buy account holder. Write method display which will display name and Balanceamt. This application will be classlibrary.
+//Create console application and use this class library. Create at least two object of account class and deposit and withdraw money.
+
+using System;
+using AccLib;
+
+namespace Account_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Press Enter To Start The App.");
+            //User 1 Object
+            Console.WriteLine("\nEnter 1st User Name:");
+            Account a1 = new Account(Console.ReadLine());       //obj 2/user 2 initialised
+            a1.disp();                                          //initial disp no balance
+            Console.Write("Deposit in acc 2 \nAmount : ");      //Depositing amount 
+            a1.deposit(double.Parse(Console.ReadLine()));
+            Console.WriteLine("Withdraw from acc 1\nAmount : ");    //Withdrawing amount
+            a1.withdraw(double.Parse(Console.ReadLine()));
+
+            //User 2 Object
+            Console.WriteLine("Enter 2nd User Name:");
+            Account a2 = new Account(Console.ReadLine());       //obj 2/user 2 initialised
+            a2.disp();
+            Console.Write("Deposit in acc 2 \nAmount : ");
+            a2.deposit(double.Parse(Console.ReadLine()));
+            Console.WriteLine("Withdraw from acc 2\nAmount : ");
+            a2.withdraw(double.Parse(Console.ReadLine()));
+            Console.ReadLine();
+        }       
+    }
+}
