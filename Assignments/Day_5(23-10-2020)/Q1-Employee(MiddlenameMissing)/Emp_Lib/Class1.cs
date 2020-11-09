f0da387f862bf0368485d240d592ef7ea786fb7b
Sumit S Chaure <sumitsc.work@gmail.com>
2020-11-09 22:18:57 +0530

    Q1 Folder

---
+++ Assignments/Day_5(23-10-2020)/Q1-Employee(MiddlenameMissing)/Emp_Lib/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emp_Lib
+{
+    public class Employee
+    {
+        /*static Employee()
+        {
+            Console.WriteLine("\n-----------------------------------------------------");
+            Console.WriteLine("\n****************Corporation Of Unknowns***************\n");
+            Console.WriteLine("-----------------------------------------------------\n");
+        }*/
+
+        string Fname, Mname, Lname;
+
+        public Employee(string Fname, string Mname, string Lname)               //employee method ttaking in all three characters
+        {
+            this.Fname = Fname;
+            this.Mname = Mname;
+            this.Lname = Lname;
+            //Console.WriteLine("Construtor Of 3 parameters");              //here we can print this line to show that app is calling three arg const
+        }
+
+        public Employee(string Fname, string Lname)                             //employee method taking in only first & last name in employee class
+        {
+            this.Fname = Fname;
+            this.Lname = Lname;
+            //Console.WriteLine("Construtor Of 2 parameters");              //here we can print this line to show that app is calling 2 arg const
+        }
+        
+        public void disp()
+        {
+            if (this.Mname == null)                                                     //if middle name is not provide then the retunrn print will display only first & last name
+                Console.WriteLine(" Employee Name : {0} {1}", Fname, Lname);        
+            else                                                                        //e;se the print dispaly all the three charcters enterd by the user
+                Console.WriteLine("Employee Name : {0} {1} {2}", Fname, Mname, Lname);      
+        }
+    }
+}
---
+++ Assignments/Day_5(23-10-2020)/Q1-Employee(MiddlenameMissing)/Program.cs
@@ -0,0 +1,44 @@
+﻿//Q1. Create a class Employee having member variable Firstname, Lastname, Middlename.let your application create object even if user does not enter Middlename. Create two constructor one take parameter for Firstname, Lastname and second constructor which will take all the parameter. Write display method which will display detail of the object.
+
+using System;
+using Emp_Lib;
+
+namespace Q1_Employee_MiddlenameMissing_
+{
+    class Program
+    {
+        static Program()
+        {
+            Console.WriteLine("\n-----------------------------------------------------");
+            Console.WriteLine("\n****************Corporation Of Unknowns***************\n");
+            Console.WriteLine("-----------------------------------------------------\n");
+        }
+
+        static void Main(string[] args)
+        {
+            int ch;
+            string Fname, Mname, Lname;
+            Employee e1;
+            //Employee e2;
+            do {
+                Console.Write("\n        Enter Name Of Employee \n\nFirst Name : ");
+                Fname = Console.ReadLine();
+                Console.Write("Middle Name : ");
+                Mname = Console.ReadLine();
+                Console.Write("Last Name : ");
+                Lname = Console.ReadLine();
+               // Console.WriteLine("{0} {1} {2}", Fname,Mname, Lname);
+
+                if(Mname == null)
+                     e1 = new Employee(Fname,Lname);
+                else
+                     e1 = new Employee(Fname, Mname ,Lname);
+                e1.disp();
+                //e2.disp();
+                Console.WriteLine("\n\nEnter 0 to Exit\n\nElse 1 to Continue The Application...");
+                Console.WriteLine("\n-----------------------------------------------------\n");
+                ch=int.Parse(Console.ReadLine());
+            } while (ch != 0);
+        }
+    }
+}
