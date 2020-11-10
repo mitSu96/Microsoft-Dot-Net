a55344b0299748a902cb6864ae34e12b7e120c9e
Sumit S Chaure <sumitsc.work@gmail.com>
2020-11-10 21:39:39 +0530

    Array Operations Library Class (Not Used)

---
+++ Assignments/Day_6(24-10-2020)/Array_Opertions_Lib/Class1.cs
@@ -0,0 +1,27 @@
+﻿//Q3. Accept data in an array and print sum  also store square of each element in new array.
+//Q4. Accept data in an array pass it to static method let this method store cube of each element , modify original array
+//Q5. Accept data in an array and print average.
+
+using System;
+
+namespace Array_Opertions_Lib
+{
+    public class Arrayoperations
+    {
+        int[] arr;
+        int size;
+
+        public Arrayoperations(int size)
+        {
+            this.size = size;
+            arr = new int[size];
+            Console.WriteLine("Enter The Numbers Of Array");
+            for (int i = 0; i < size; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+        
+}
+
